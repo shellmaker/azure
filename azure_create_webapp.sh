08d51c975e70e33183b06b9010a36950d5762803
Shellmaker.io (eboxx group) <30957975+shellmaker@users.noreply.github.com>
2017-12-08 12:33:21 -0200

    Create azure_create_webapp.sh

---
+++ azure_create_webapp.sh
@@ -0,0 +1,6 @@
+# Create an Azure AppService that we can host any number of web apps within
+az appservice plan create -n MyAppServicePlan -g MyResourceGroup
+
+# Create Two Web Apps within the AppService (note: name param must be a unique DNS entry)
+az webapp create -n MyWebApp43432 -g MyResourceGroup --plan MyAppServicePlan 
+az webapp create -n MyWebApp43433 -g MyResourceGroup --plan MyAppServicePlan
